57983d15789135449b71adaaecac1a00365ff96f
rammohanmiryala <rammohanmiryala@hotmail.com>
2021-10-02 21:40:28 +0930

    fixed time intervel

--- script.js
+++ script.js
@@ -64,37 +64,7 @@ var myquestions = [{
     },
 
 ]
-var timerInterval = ""
 var secondsLeft = 60;
-
-
-
-function starttimer() {
-    timerInterval = setTimeout(function () {
-        secondsLeft--;
-        var timer = quiztimerEl.textContent = " Time Left : " + secondsLeft;
-        if (secondsLeft === 0) {
-            clearTimeout(timerInterval);
-            // sendMessage();
-
-        }
-
-    }, 1000);
-}
-
-function reducetimer() {
-    setInterval(function () {
-        secondsLeft--;
-        quiztimerEl.textContent = " Time Left : " + (secondsLeft - 10);
-        if (secondsLeft === 0) {
-            setInterval(timerInterval);
-            // sendMessage();
-
-        }
-
-    }, 1000);
-
-}
 
 function startquiz() {
     var x = document.querySelector("#quizcard");
@@ -111,7 +81,24 @@ function startquiz() {
     }
 }
 
+function starttimer() {
+    timerInterval = setInterval(function () {
+        secondsLeft--;
+        quiztimerEl.textContent = " Time Left : " + secondsLeft;
+        if (secondsLeft === 0) {
+            clearInterval(timerInterval);
+        }
 
+    }, 1000);
+}
+
+function reducetimer() {
+    secondsLeft = secondsLeft -5
+}
+
+function scorepage(){
+    location.href = "http://192.168.1.126:5500/Javascript-Quiz/index.html"
+}
 
 // qustion one-----------------------------------------------------------
 function startgame() {
@@ -130,8 +117,9 @@ function startgame() {
 
         } else {
             textareaEl.innerHTML = "wrong"
-
+            reducetimer()
             newqustion1()
+
 
         }
 
@@ -145,8 +133,9 @@ function startgame() {
 
         } else {
             textareaEl.innerHTML = "wrong"
-            newqustion1()
             reducetimer()
+            newqustion1()
+
         }
 
     })
@@ -159,8 +148,9 @@ function startgame() {
 
         } else {
             textareaEl.innerHTML = "wrong"
-            newqustion1()
             reducetimer()
+            newqustion1()
+
 
         }
 
@@ -174,12 +164,14 @@ function startgame() {
 
         } else {
             textareaEl.innerHTML = "wrong"
-            newqustion1()
             reducetimer()
+            newqustion1()
+
 
         }
 
     })
+
 
 }
 // qustion two-----------------------------------------------------------
@@ -190,8 +182,6 @@ function newqustion1() {
     option1El.innerHTML = myquestions[1].choise1
     option2El.innerHTML = myquestions[1].choise2
     option3El.innerHTML = myquestions[1].choise3
-
-
     option0El.addEventListener('click', function () {
         option0El.value = 0
         if (myquestions[1].answer == option0El.value) {
@@ -252,7 +242,6 @@ function newqustion1() {
         }
 
     })
-
 }
 // qustion three-----------------------------------------------------------
 function newqustion2() {
@@ -262,7 +251,6 @@ function newqustion2() {
     option1El.innerHTML = myquestions[2].choise1
     option2El.innerHTML = myquestions[2].choise2
     option3El.innerHTML = myquestions[2].choise3
-
 
     option0El.addEventListener('click', function () {
         option0El.value = 0
@@ -326,15 +314,157 @@ function newqustion2() {
     })
 
 }
+
+
 // qustion four-----------------------------------------------------------
 function newqustion3() {
+    quizquestionEl.innerHTML = myquestions[3].question
+    option0El.innerHTML = myquestions[3].choise0
+    option1El.innerHTML = myquestions[3].choise1
+    option2El.innerHTML = myquestions[3].choise2
+    option3El.innerHTML = myquestions[3].choise3
+    
+    option0El.addEventListener('click', function () {
+        option0El.value = 0
+        if (myquestions[3].answer == option0El.value) {
+            textareaEl.innerHTML = "correct"
+            score++;
+            newqustion4()
+
+        } else {
+            textareaEl.innerHTML = "wrong"
+            reducetimer()
+            newqustion4()
+
+
+        }
+
+    })
+    option1El.addEventListener('click', function () {
+        option1El.value = 1
+        if (myquestions[3].answer == option1El.value) {
+            textareaEl.innerHTML = "correct"
+            score++;
+            newqustion1()
+
+        } else {
+            textareaEl.innerHTML = "wrong"
+            reducetimer()
+            newqustion4()
+
+        }
+
+    })
+    option2El.addEventListener('click', function () {
+        option2El.value = 2
+        if (myquestions[3].answer == option2El.value) {
+            textareaEl.innerHTML = "correct"
+            score++;
+            newqustion4()
+
+        } else {
+            textareaEl.innerHTML = "wrong"
+            reducetimer()
+            newqustion4()
+
+
+        }
+
+    })
+    option3El.addEventListener('click', function () {
+        option3El.value = 3
+        if (myquestions[3].answer == option3El.value) {
+            textareaEl.innerHTML = "correct"
+            score++;
+            newqustion4()
+
+        } else {
+            textareaEl.innerHTML = "wrong"
+            reducetimer()
+            newqustion4()
+
+
+        }
+
+    })
 
 }
 
-// qustion four-----------------------------------------------------------
+// qustion five-----------------------------------------------------------
 function newqustion4() {
+    quizquestionEl.innerHTML = myquestions[4].question
+    option0El.innerHTML = myquestions[4].choise0
+    option1El.innerHTML = myquestions[4].choise1
+    option2El.innerHTML = myquestions[4].choise2
+    option3El.innerHTML = myquestions[4].choise3
+
+    option0El.addEventListener('click', function () {
+        option0El.value = 0
+        if (myquestions[3].answer == option0El.value) {
+            textareaEl.innerHTML = "correct"
+            score++;
+            scorepage()
+
+        } else {
+            textareaEl.innerHTML = "wrong"
+            reducetimer()
+            scorepage()
+
+
+        }
+
+    })
+    option1El.addEventListener('click', function () {
+        option1El.value = 1
+        if (myquestions[3].answer == option1El.value) {
+            textareaEl.innerHTML = "correct"
+            score++;
+            scorepage()
+
+        } else {
+            textareaEl.innerHTML = "wrong"
+            reducetimer()
+            scorepage()
+
+        }
+
+    })
+    option2El.addEventListener('click', function () {
+        option2El.value = 2
+        if (myquestions[3].answer == option2El.value) {
+            textareaEl.innerHTML = "correct"
+            score++;
+            scorepage()
+
+        } else {
+            textareaEl.innerHTML = "wrong"
+            reducetimer()
+            scorepage()
+
+
+        }
+
+    })
+    option3El.addEventListener('click', function () {
+        option3El.value = 3
+        if (myquestions[3].answer == option3El.value) {
+            textareaEl.innerHTML = "correct"
+            score++;
+            scorepage()
+
+        } else {
+            textareaEl.innerHTML = "wrong"
+            reducetimer()
+            scorepage()
+
+
+        }
+
+    })
 
 }
+
+
 
 
 startquizBtn.addEventListener("click", startquiz)
